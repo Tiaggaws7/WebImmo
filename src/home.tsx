e267a7eb4cc60376270bcb7c800ca7b3a4b3d6cb
Tiaggaws7 <tiagobuil.tb@gmail.com>
2024-11-22 18:43:11 +0100

    removed unused import

--- src/home.tsx
+++ src/home.tsx
@@ -7,7 +7,6 @@ import house1 from './assets/house1.jpg'
 import house2 from './assets/house2.jpg'
 import house3 from './assets/house3.jpg'
 import house4 from './assets/house4.jpg'
-import houseGwada from './assets/houseGwada.jpg'
 
 function Button({ 
   children, 
