520ff998a70e65c5ce43f8b740867d464cdb5495
Tiaggaws7 <tiagobuil.tb@gmail.com>
2025-01-31 14:25:24 +0100

    fix unused variable

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { ArrowRight, User } from "lucide-react"
+import { ArrowRight } from "lucide-react"
 
 const NavigationPage = () => {
   const navigationLinks = [
